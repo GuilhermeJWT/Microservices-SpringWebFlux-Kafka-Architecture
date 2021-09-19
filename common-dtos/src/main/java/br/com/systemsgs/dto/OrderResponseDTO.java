65416dae5b4a2a9def1c51f0f2e0fb8faa3b69ad
GuilhermeJWT <guiromanno@gmail.com>
2021-09-19 10:56:35 -0300

    criando nossas classes de DTO

---
+++ common-dtos/src/main/java/br/com/systemsgs/dto/OrderResponseDTO.java
@@ -0,0 +1,19 @@
+package br.com.systemsgs.dto;
+
+import br.com.systemsgs.event.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderResponseDTO {
+
+    private Integer userId;
+    private Integer productId;
+    private Integer amount;
+    private Integer orderId;
+    private OrderStatus orderStatus;
+
+}
---
+++ common-dtos/src/main/java/br/com/systemsgs/dto/PaymentRequestDTO.java
@@ -0,0 +1,16 @@
+package br.com.systemsgs.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentRequestDTO {
+
+    private Integer orderId;
+    private Integer userId;
+    private Integer amount;
+
+}
